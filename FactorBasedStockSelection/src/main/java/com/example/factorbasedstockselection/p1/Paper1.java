57d2a9eec7ae09e4c6c88d863e5694958d0c37e0
dwd <weidong.du@foxmail.com>
2024-04-19 16:53:38 +0800

    加入原文信息

--- FactorBasedStockSelection/src/main/java/com/example/factorbasedstockselection/p1/Paper1.java
+++ FactorBasedStockSelection/src/main/java/com/example/factorbasedstockselection/p1/Paper1.java
@@ -6,7 +6,17 @@ package com.example.factorbasedstockselection.p1;
 public class Paper1 {
     /*
     基于插值排序标准化变量法和复杂变量的平衡分离树的多因子选股模型
+
+    文献来源：
+    Building multi-factor stock selection models
+    using balanced split regression trees with sorting normalisation and hybrid variables.
+
+    Yeh, I-C., Lien, C-H. And Ting, T-M. Int. J. Foresight and Innovation Policy, Vol. 10, No. 1, 2015
+
+    论文原文地址:
+    https://www.researchgate.net/publication/281341457_Building_multi-factor_stock_selection_models_using_balanced_split_regression_trees_with_sorting_normalisation_and_hybrid_variables
      */
+
 
 
 }
